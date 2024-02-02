290aef244ffc35abdfaf9788244e6f3c0c435f1c
Laura-Lepage <lauralepage02@hotmail.com>
2024-02-02 14:18:10 +0100

    suite du vendredi

--- forthemiamers/scripts/main.js
+++ forthemiamers/scripts/main.js
@@ -1,24 +1,25 @@
-let name = document.querySelector("#names").value
-let quantity = document.querySelector("#quantity").value
+let nom = document.querySelector("#names")
+let quantity = document.querySelector("#quantity")
 let product = document.querySelector("input")
 let button = document.querySelector("button")
 let ul = document.querySelector("ul")
 let p = document.querySelector("p")
+let tableau = []
 
 function Checklist(){
-    if (ul.firstChild){
+    if (ul.childElementCount != 0){
         p.innerHTML = ``
     } else {
-        p.innerHTML = `Votre liste est vide`
+        p.innerHTML = `Votre liste est vide` 
     }
-
 }
 
 button.addEventListener('click', function(){
+    tableau.push({nom:nom.value, quantité:quantity.value, produit: product.value})
     
     ul.innerHTML+=
     `
-    <li>${name} ${quantity} ${product.value}<button class="list">X</button></li>
+    <li>${tableau[tableau.length-1].nom} ${tableau[tableau.length-1].quantité} ${tableau[tableau.length-1].produit}<button class="list">X</button></li>
     `
     product.value = ""
     
@@ -28,21 +29,8 @@ button.addEventListener('click', function(){
     boutons.forEach(function(bouton){
         bouton.addEventListener('click', function(){
             bouton.parentElement.remove()
+            p = document.querySelector("p")
             Checklist()
-
         })     
-    })
-
-
-
-
-
-
-
-    ul.innerHTML =
-    `<li>${name} ${quantity}x ${product.value}</li>`
-
-    Checklist()
-
-    
+    })    
 })
--- guessinggame/scripts/main.js
+++ guessinggame/scripts/main.js
@@ -1,32 +1,44 @@
-let number = parseInt(document.querySelector("input").value)
+let number = document.querySelector("input")
 let button = document.querySelector("button")
 let p = document.querySelector("p")
 let nombreEssais = 0
+let randomNumba = Math.random()*100
+let arrondiNumba = Math.round(randomNumba)
+console.log(arrondiNumba);
 
-
-
-function randomNumber(){
-    let min = 0
-    let max = 100
-    let randomNumb = Math.floor(Math.random() * (max - min)) + min;
-    return(randomNumb)
+function nombreMagique(nombreUtilisateur) {
+    nombreEssais++
+    if (nombreEssais < 5){
+        if (nombreUtilisateur < arrondiNumba){
+            p.innerHTML = `Entrer un nombre plus grand`
+            setTimeout(function(){
+                p.innerHTML = ``
+            }, 2000)
+        } else if (nombreUtilisateur > arrondiNumba){
+            p.innerHTML = `Entrer un nombre plus petit`
+            setTimeout(function(){
+                p.innerHTML = ``
+            }, 2000)
+        } else {
+            p.innerHTML = `Félicitations, tu as gagné en ${nombreEssais} essai(s)!`
+            document.querySelector(".win").style.display = "inline"
+            setTimeout(function(){
+                p.innerHTML = ``
+                document.querySelector(".win").style.display = "none"
+            }, 10000)
+        }
+    } else {
+        p.innerHTML = `Toutes les chances sont épuisées`
+        document.querySelector(".loose").style.display = "inline"
+        setTimeout(function(){
+            p.innerHTML = ``
+            document.querySelector(".loose").style.display = "none"
+        }, 10000)
+        
+    }
+    number.value = ""
 }
-console.log(randomNumber());
 
 button.addEventListener('click', function(){
-    nombreEssais++
-    if (number == randomNumber()){
-        p.innerHTML = `Félicitations, tu as gagné en ${nombreEssais} essais!`
-    } else {
-        if (number>randomNumber()){
-            p.innerHTML = `Entrer un nombre plus petit`
-        } else {
-            p.innerHTML = `Entrer un nombre plus grand`
-        }
-    }
-
-    if (nombreEssais<=5){
-
-
-    }
+    nombreMagique(number.value)
 })
